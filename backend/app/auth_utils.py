dbf480316c266a2228ff0d293bf47d3ae1cea654
true1853 <true@T-book.localdomain>
2025-06-24 12:44:10 +0300

    Fix: main.py

--- backend/app/auth_utils.py
+++ backend/app/auth_utils.py
@@ -13,7 +13,7 @@ from app.database import get_db
 from app.security import verify_password 
 from app.config import settings 
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/token")
 
 
 ACCESS_TOKEN_EXPIRE_MINUTES = 30 
